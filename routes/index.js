f6c6fc544037eb665ce52fe6b9dcf0d70c9b0f54
Oswaldo.Otero <oswaldo.otero@BOATSGROUPs-MacBook-Pro-8.local>
2018-11-05 10:49:37 -0500

    I think the logic is done and this is ready to be deploy on the server.

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,35 @@ router.get('/', function(req, res, next) {
 });
 /* GET New User page. */
 router.get('/newuser', function(req, res) {
+    //console.log(req);
     res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    var mongo = require('mongodb');
+    var monk = require('monk');
+    var db = monk('localhost:27017/thedailyjants');
+
+
+    var userEmail = req.body.useremail;
+
+    // Set our collection
+    var collection = db.get('subscribers');
+
+    // Submit to the DB
+    collection.insert({
+        "email": userEmail
+    }, function(err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        } else {
+            // And forward to success page
+            console.log(doc);
+            res.redirect("newuser");
+        }
+    });
 });
 module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -15,13 +15,29 @@ var appRouter = function(app) {
 
         var thejant = db.get('jants');
 
-        thejant.findOne({ sent: 0 }, function(e, docs) {
 
-
+        thejant.find({ sent: 0 }, function(e, docs) {
+            var total_records = docs.length;
+            console.log(total_records);
             //res.json(docs);
-            if (e) {
-                throw e;
+            if (total_records === 0) {
+                //throw e;
+                var myquery = { sent: 1 };
+                var newvalues = { $set: { sent: 0 } };
+                thejant.update(myquery, newvalues, { multi: true }, function(err, res) {
+                    if (err) throw err;
+                    console.log(res);
+                    //db.close();
+                    res.redirect("dailyjant");
+                });
             } else {
+                var total_records = docs.length;
+
+                // console.log(total_records);
+                //res.json(docs);
+                var todaysjant = docs[Math.floor(Math.random() * total_records)];
+                //console.log(todaysjant.jant);
+                //thejant.findOne({ sent: 0 }, function(e, docs) {
                 'use strict';
                 const nodemailer = require('nodemailer');
 
@@ -38,30 +54,48 @@ var appRouter = function(app) {
                             pass: account.pass // generated ethereal password
                         }
                     });
+                    var emails = db.get('subscribers');
+                    emails.find({}, {}, function(e, emails) {
+                        //console.log(docs);
+                        emails.forEach(function(value) {
+                            //console.log(value.email);
+                            //console.log(emails);
+                            // setup email data with unicode symbols
+                            let mailOptions = {
+                                from: '"Oswaldo Otero" <oswaldo@trinityopensolutions.com>', // sender address
+                                to: '' + value.email + '', // list of receivers
+                                subject: 'The Wednesday Jant: #' + todaysjant.id + '', // Subject line
+                                text: 'Josh Cordes said: -->>' + todaysjant.jant + '<<--', // plain text body
+                                html: 'Josh Cordes said: <b>' + todaysjant.jant + '</b>' // html body
+                            };
 
-                    // setup email data with unicode symbols
-                    let mailOptions = {
-                        from: '"Oswaldo Otero" <oswaldo@trinityopensolutions.com>', // sender address
-                        to: 'oswaldo@trinityopensolutions.com', // list of receivers
-                        subject: 'The Wednesday Jant: #' + docs.id + '', // Subject line
-                        text: 'Josh Cordes said: -->>' + docs.jant + '<<--', // plain text body
-                        html: 'Josh Cordes said: <b>' + docs.jant + '</b>' // html body
-                    };
+                            // send mail with defined transport object
+                            transporter.sendMail(mailOptions, (error, info) => {
+                                if (error) {
+                                    return console.log(error);
+                                }
+                                console.log('Message sent: %s', info.messageId);
+                                // Preview only available when sending through an Ethereal account
+                                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-                    // send mail with defined transport object
-                    transporter.sendMail(mailOptions, (error, info) => {
-                        if (error) {
-                            return console.log(error);
-                        }
-                        console.log('Message sent: %s', info.messageId);
-                        // Preview only available when sending through an Ethereal account
-                        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
+                            });
+                        });
                     });
+
+
+
                 });
 
 
-                res.json(docs);
+                res.json(todaysjant);
+                //Here we are going to update the database...
+                var myquery = { id: todaysjant.id };
+                var newvalues = { $set: { sent: 1 } };
+                thejant.update(myquery, newvalues, function(err, res) {
+                    if (err) throw err;
+                    console.log("1 document updated");
+                    db.close();
+                });
             }
 
 
